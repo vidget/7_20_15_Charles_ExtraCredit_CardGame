21398c2b8c4c74a8390b452387b3796b07a3ca0c
Vidget <vidget@yahoo.com>
2015-07-20 16:53:49 -0400

    Multiplayer Card Game

---
+++ 7_20_15_Charles_ExtraCredit_CardGame/Cards.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_20_15_Charles_ExtraCredit_CardGame
+{
+    class Cards
+    {
+        List<string> userHand = new List<string>();//holds the userHand strings
+        public string playerName { get; set; }//holds the player NAME
+        
+        //creates a dictionary that holds 52 card strings with unicode card symbol \u0003 = Hearts \u0004 = Diamonds \u0005 = Clubs \u0006 = Spades
+        Dictionary<int, string> cards = new Dictionary<int, string>()
+            { 
+                { 0,"A\u0006"},
+                { 1,"2\u0006"},
+                { 2,"3\u0006"},
+                { 3,"4\u0006"},
+                { 4,"5\u0006"},
+                { 5,"6\u0006"},
+                { 6,"7\u0006"},
+                { 7,"8\u0006"},
+                { 8,"9\u0006"},
+                { 9,"10\u0006"},
+                { 10,"J\u0006"},
+                { 11,"Q\u0006"},
+                { 12,"K\u0006"},
+                { 13,"A\u0005"},
+                { 14,"2\u0005"},
+                { 15,"3\u0005"},
+                { 16,"4\u0005"},
+                { 17,"5\u0005"},
+                { 18,"6\u0005"},
+                { 19,"7\u0005"},
+                { 20,"8\u0005"},
+                { 21,"9\u0005"},
+                { 22,"10\u0005"},
+                { 23,"J\u0005"},
+                { 24,"Q\u0005"},
+                { 25,"K\u0005"},
+                { 26,"A\u0003"},
+                { 27,"2\u0003"},
+                { 28,"3\u0003"},
+                { 29,"4\u0003"},
+                { 30,"5\u0003"},
+                { 31,"6\u0003"},
+                { 32,"7\u0003"},
+                { 33,"8\u0003"},
+                { 34,"9\u0003"},
+                { 35,"10\u0003"},
+                { 36,"J\u0003"},
+                { 37,"Q\u0003"},
+                { 38,"K\u0003"},
+                { 39,"A\u0004"},
+                { 40,"2\u0004"},
+                { 41,"3\u0004"},
+                { 42,"4\u0004"},
+                { 43,"5\u0004"},
+                { 44,"6\u0004"},
+                { 45,"7\u0004"},
+                { 46,"8\u0004"},
+                { 47,"9\u0004"},
+                { 48,"10\u0004"},
+                { 49,"J\u0004"},
+                { 50,"Q\u0004"},
+                { 51,"K\u0004"},
+            };
+
+        public Cards()
+        {
+            playerName = "";
+
+        }
+
+
+        public void addCardtoHand(int cardNumber)// List<string> userHand)
+        {
+            userHand.Add(cards[cardNumber]);
+        }
+
+
+
+        public int drawCard()
+        {
+            //generates a random number between 0-51
+            Random random = new Random((int)DateTime.Now.Ticks);
+            int randomNumber = random.Next(0, 52);
+            return randomNumber;
+
+        }
+
+        public void removeCard(int userCard, int[] deck)//removes the card the user drew from the deck by setting the value of the deck array from 1 to 0
+        {
+            deck[userCard] = 0;
+        }
+
+        public void displayHand()//(List<string> userHand)//displays all the strings stored in the userHand list
+        {
+            Console.WriteLine("\n"+playerName+" your Hand:\n");
+
+            for (int i = 0; i < userHand.Count; i++)
+            {
+                Console.Write(" " + userHand[i]);
+            }
+            Console.WriteLine();
+
+
+        }
+
+        public bool checkIfAvailableCard(int tempCard, int[] deck)//checks to see if the deck array contains a 1 or 0 taken or not, returns a BOOL
+        {
+            if (deck[tempCard] == 1)
+                return false;//card is taken
+            else
+                return true;// not taken
+        }
+
+    }
+}
+
+
---
+++ 7_20_15_Charles_ExtraCredit_CardGame/Program.cs
@@ -0,0 +1,88 @@
+﻿using _7_20_15_Charles_ExtraCredit_CardGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Gregory Scott Norris - ExperienceIT 7-16-15
+//Exercise 2: CardGame adds players 
+//Write a program to create a standard 52 card deck and then deal a random hand to a  several players player. Once a card is dealt then it can't be used again.
+
+namespace _7_16_15_ExperIT_Charles_ExtraCredit_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a INT array that holds a deck of 52 cards (1 = card in deck, 0 = card already drawn)
+            int[] deck = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+           // List<string> userHand = new List<string>();//userhand holds the string of the card number & suit drawn by the user
+            List<Cards> gameUser = new List<Cards>();//not really used in this program, but will be used in the multiplayer version. 
+            getNumOfPlayers(gameUser);//calls the method to load the GAMEUSER list with players
+            drawCards(gameUser,deck);//calls the drawCards sends the gameUser & the deck
+            Console.ReadLine();//pause to read
+        }
+
+
+
+
+
+
+
+
+        public static void getNumOfPlayers(List<Cards> gameUser)
+        {
+            int numberOfPlayers = 0;//user input number of cards to draw
+            Console.WriteLine("\n How many PLAYERS would you like to play");//prompts user for number of cards to draw
+            Console.Write(" >");//user prompt
+            numberOfPlayers = int.Parse(Console.ReadLine());//takes in the number of users, used in FORLOOP while drawing cards
+            Console.Clear();
+            //adds the selected number of users to the game 
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                Cards tempPlayer = new Cards();//creates a CARDS class called tempPlayer and puts it into the LIST GAMEUSER
+                gameUser.Add(tempPlayer);
+            }
+
+            string tempName;//temp User Name
+            for (int i = 0; i < gameUser.Count; i++)
+            {
+                Console.WriteLine("\n Enter Player #" + (i + 1) + "'s Name");
+                Console.Write(" >");//user prompt
+                tempName = Console.ReadLine();
+                gameUser[i].playerName = tempName;
+                Console.Clear();
+            }
+        }
+        public static void drawCards(List<Cards>gameUser, int[] deck)
+        {
+            int tempCard;//holds the drawn card
+            int numberOfDraws = 0;//user input number of cards to draw
+            bool isCardGone = true;//determines if a card is available or already taken from the deck
+
+            Console.WriteLine("\n How many cards would you like to draw?");//prompts user for number of cards to draw
+            Console.Write(" >");//user prompt
+            numberOfDraws = int.Parse(Console.ReadLine());//parses in the user input for number of draws
+            Console.Clear();
+
+            for (int x = 0; x < gameUser.Count; x++)
+            {
+                for (int i = 0; i < numberOfDraws; i++)//loops through the selecting cards, adding to hand and removing from the deck
+                {
+                    do//This will loop until the random number generates an available card.
+                    {
+                        tempCard = gameUser[x].drawCard(); //drawCard Method generates a random number between 1&52
+                        isCardGone = gameUser[x].checkIfAvailableCard(tempCard, deck);//sends deck & drawn card to method to check if card drawn is still in deck, returns true if card has already been drawn
+                    } while (isCardGone == true); ////check that card hasn't been drawn yet
+
+                    gameUser[x].addCardtoHand(tempCard);//add the card to the userHand
+                    gameUser[x].removeCard(tempCard, deck);//remove card from the deck
+                }
+                gameUser[x].displayHand();//display the users hand.
+            }
+        }
+    }
+}
